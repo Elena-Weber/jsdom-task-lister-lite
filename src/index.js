b9c068b55fe8acad64f1bc2b76e418aab0183ba8
Elena-Weber <crochetmiracles@gmail.com>
2021-05-25 22:52:23 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let task = document.getElementById('create-task-form');
+  let createNewTask = document.getElementById('new-task-description');
+  let taskItem = document.getElementById('tasks');
+  task.addEventListener('submit', addingToList);
 });
+
+const addingToList = event => {
+  event.preventDefault();
+  let newTaskDescription = document.getElementById("new-task-description");
+  let newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+let appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
